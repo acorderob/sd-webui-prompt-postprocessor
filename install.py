0e884e94dbbc27d00b976f5a291edccf2b9237e8
Antonio Cordero Balcazar <antonio.cordero@gmail.com>
2026-06-04 18:03:02 +0200

    * Fix a problem properly installing dependencies in all A1111 hosts.

--- install.py
+++ install.py
@@ -3,11 +3,9 @@ from pathlib import Path
 requirements_filename = str(Path(__file__).resolve().parent / "requirements.txt")
 
 try:
-    from modules.launch_utils import requirements_met, run_pip  # A1111
-
-    if not requirements_met(requirements_filename):
-        run_pip(f'install -r "{requirements_filename}"', "requirements for Prompt Post-Processor")
+    from modules.launch_utils import run_pip  # , requirements_met  # A1111
+    #if not requirements_met(requirements_filename): # fails in all hosts due to ruamel.yaml or not having specified versions
+    run_pip(f'install -r "{requirements_filename}"', "requirements for Prompt Post-Processor")
 except ImportError:
     import launch
-
     launch.run_pip(f'install -r "{requirements_filename}"', "requirements for Prompt Post-Processor")
